95635fc6e0778d395f20dab4301b2700059f09a2
ReidL1307 <79946812+ReidL1307@users.noreply.github.com>
2022-04-09 11:47:58 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+﻿import React, { useState } from 'react';
+import "./App.css";
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const Uberduck = require('./uberDuck');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = createTheme();
+
+export default function App() {
+    const [sound, setSound] = useState("");
+    async function getQuote() {
+        var quote;
+        await fetch('https://api.kanye.rest/')
+            .then(response => response.json())
+            .then(quotes => { quote = quotes.quote + "..." });
+        console.log(quote);
+        const ud = new Uberduck("pub_dbqzdjstpysasbliys", "pk_a47ab9f4-6644-429d-92f3-23efea762082");
+        const synth = ud.synthesize(quote, "kanye-west-rap");
+        await synth.then(value => {
+            const x = value;
+            setSound(x.path);
+        }).catch(err => {
+            console.log(err);
+        });
+        const audio = new Audio(sound);
+        audio.play();
+    }
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <main>
+                <div className="butt1">
+                <Typography variant="h1" align="center">
+                artificial yeezus
+                    </Typography>
+                    </div>
+            <div align="center" className="butt2">
+                    <Button align="center" onClick={() => {
+                        
+                        getQuote();
+
+                        
+
+                    }}>
+                    <h1>
+                        Seek Enlightenment
+                    </h1>
+                    </Button>
+                </div>
+                </main>
+        </ThemeProvider>
+    );
 }
-
-export default App;
